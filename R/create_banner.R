4ffdf36b48c1eba7ee3a7e77d5a11bc2d8656e1c
TimTeaFan <github@e-niversity.com>
2023-01-26 01:16:16 +0100

    fix: write no of followers to json

--- R/create_banner.R
+++ R/create_banner.R
@@ -46,8 +46,8 @@ twitter_followers_tbl <- readRDS("data/twitter_followers.rds")
 
 # write latest follower data as json
 no_followers <- twitter_followers_tbl[nrow(twitter_followers_tbl), "followers"]
-no_followers_json = toJSON(list(data = list(followers = no_followers)))
-write(no_followers_json, "data/followers.json")
+no_followers_json = jsonlite::toJSON(list(data = list(followers = no_followers)))
+write(no_followers_json, "followers")
 
 # get last date in historical follower data
 last_tbl_date <- twitter_followers_tbl %>%
